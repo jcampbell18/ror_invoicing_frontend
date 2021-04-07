031ea518d1a8f6bcfd869387980fb76dbde84209
Jason Campbell <jcryuu@gmail.com>
2021-04-06 20:23:43 -0700

    updated vue component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+    <Header />
+    <Aside />
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Aside from './components/Aside.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Header,
+    Aside,
+    Main,
+    Footer,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#container {
+  width: 96%;
+  background: var(--off-white);
+  border: 4px solid var(--secondary-color);
+  border-radius: 10px;
+  box-shadow: rgba(66,57,58,0.4) 4px 4px 4px;
+
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
 }
 </style>
