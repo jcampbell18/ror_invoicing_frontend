024d7278499e57c59d1225fdc46d833925dae70b
Jason Campbell <jcryuu@gmail.com>
2021-04-12 19:09:15 -0700

    moved images into nav directory

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,31 +3,31 @@
     <ol>
       <li>
         <a href="">
-          <img src="../assets/icons/512x512/dashboard.png" alt="Dashboard" title="Dashboard"/>
+          <img src="src/assets/icons/512x512/nav/dashboard.png" alt="Dashboard" title="Dashboard"/>
           <h6>Dashboard</h6>
         </a>
       </li>
       <li>
         <a href="">
-          <img src="../assets/icons/512x512/content.png" alt="Content" title="Content"/>
+          <img src="src/assets/icons/512x512/nav/content.png" alt="Content" title="Content"/>
           <h6>Content</h6>
         </a>
       </li>
       <li>
         <a href="">
-          <img src="../assets/icons/512x512/user.png" alt="Users" title="Users"/>
+          <img src="src/assets/icons/512x512/nav/user.png" alt="Users" title="Users"/>
           <h6>Users</h6>
         </a>
       </li>
       <li>
         <a href="">
-          <img src="../assets/icons/512x512/settings.png" alt="Settings" title="Settings"/>
+          <img src="src/assets/icons/512x512/nav/settings.png" alt="Settings" title="Settings"/>
           <h6>Settings</h6>
         </a>
       </li>
       <li>
         <a href="">
-          <img src="../assets/icons/512x512/logout.png" alt="Logout" title="Logout"/>
+          <img src="src/assets/icons/512x512/nav/logout.png" alt="Logout" title="Logout"/>
           <h6>Logout</h6>
         </a>
       </li>
